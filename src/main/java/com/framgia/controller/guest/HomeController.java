aef81992d0c3f93409b46e550d32ced0d40ec588
Đoàn Quang Hòa <hoa9x3@gmail.com>
2019-07-11 09:04:53 +0700

    Update HomeController.java

--- src/main/java/com/framgia/controller/guest/HomeController.java
+++ src/main/java/com/framgia/controller/guest/HomeController.java
@@ -1,5 +1,7 @@
 package com.framgia.controller.guest;
 
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
 import com.framgia.service.ChatworkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,8 +21,52 @@ public class HomeController {
     private String chatworkRoomId;
 
     @GetMapping
-    public String dashboard(Model model){
+    public String dashboard(Model model, HttpServletRequest request){
         chatworkService.sendMessage(chatworkRoomId,"https://www.facebook.com/undefendable/photos/a.112307692132971/2534478743249175/?type=3&theater");
+        model.addAttribute("clientIP", getClientIP(request));
         return "guest/index";
     }
+    
+	public static String getClientIP(HttpServletRequest request) {
+		if (request == null) {
+			return null;
+		}
+		String ip = request.getHeader("true-client-ip");
+		if (isValidIP(ip)) {
+			return ip;
+		}
+		String xff = request.getHeader("X-Forwarded-For");
+		ip = getClientIPFromXFF(xff);
+		if (ip != null) {
+			return ip;
+		}
+		ip = request.getHeader("Proxy-Client-IP");
+		if (isValidIP(ip)) {
+			return ip;
+		}
+		ip = request.getHeader("WL-Proxy-Client-IP");
+		if (isValidIP(ip)) {
+			return ip;
+		}
+		ip = request.getRemoteAddr();
+		return ip;
+	}
+
+	private static String getClientIPFromXFF(String xff) {
+		if (StringUtils.isBlank(xff)) {
+			return null;
+		}
+		String[] ss = xff.split(",");
+		for (String ip : ss) {
+			ip = ip.trim();
+			if (isValidIP(ip)) {
+				return ip;
+			}
+		}
+		return null;
+	}
+
+	private static boolean isValidIP(String ip) {
+		return !StringUtils.isEmpty(ip) && !"unknown".equalsIgnoreCase(ip);
+	}
 }
